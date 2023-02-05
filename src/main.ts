38749d7eda4840febff793a79b790462eb9ea6e5
Fabian <fabian.klimpel2002@gmail.com>
2023-02-05 02:36:47 +0100

    new Logo

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,10 @@ import "./css/style.css";
 import App from "./App.vue";
 import router from "./router/routes";
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faHouse, faGears, faChessQueen, faBorderAll } from '@fortawesome/free-solid-svg-icons'
+import { faGear, faHouse, faGears, faChessQueen, faBorderAll } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faHouse, faGears, faChessQueen, faBorderAll)
+library.add(faGear, faHouse, faGears, faChessQueen, faBorderAll)
 
 const app = createApp(App)
 app.component('font-awesome-icon', FontAwesomeIcon)
