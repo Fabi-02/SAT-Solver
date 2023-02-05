28f0f038a39871b43ea392df3872a9046c290016
Fabian <fabian.klimpel2002@gmail.com>
2023-02-05 15:10:00 +0100

    improve Logo

--- src/components/menubar/MenuItem.vue
+++ src/components/menubar/MenuItem.vue
@@ -11,8 +11,8 @@ const isActive = computed(() => route.path === props.to)
 </script>
 
 <template>
-    <router-link :to="to" class="link px-3 py-2 rounded-md text-sm font-medium"
-        :class="{ 'active-menu-tab': isActive, 'menu-tab': !isActive }">
+    <router-link :to="to" class="link px-3 py-2 rounded-md text-sm font-medium menu-tab transition-all"
+        :class="{ 'active-menu-tab': isActive }">
         <slot />
     </router-link>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { faGear, faHouse, faGears, faChessQueen, faBorderAll } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faGear, faHouse, faGears, faChessQueen, faBorderAll)
+library.add(faGear, faHouse, faGears, faChessQueen, faBorderAll);
 
-const app = createApp(App)
-app.component('font-awesome-icon', FontAwesomeIcon)
-app.use(router)
+const app = createApp(App);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.use(router);
 app.mount("#app");
