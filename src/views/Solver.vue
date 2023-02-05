a7b740908c0a622b80b195bee215b926f2229677
Fabian <fabian.klimpel2002@gmail.com>
2023-02-05 22:09:15 +0100

    formular input test

--- src/views/Solver.vue
+++ src/views/Solver.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import ContentPage from '../components/ContentPage.vue'
-import D3Test from '../components/d3/D3Test.vue'
+import FormularInput from '../components/FormularInput.vue'
+// import D3Test from '../components/d3/D3Test.vue'
 </script>
 
 <template>
     <ContentPage name="SAT-Solver">
-        <div class="h-full rounded-lg border-4 border-dashed border-gray-200">
+        <!-- <div class="h-full rounded-lg border-4 border-dashed border-gray-200">
             <D3Test />
-        </div>
+        </div> -->
+        <FormularInput />
     </ContentPage>
 </template>
