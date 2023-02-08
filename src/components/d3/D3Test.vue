c05fe47bb0c2ee9f206d3ef929babe42f480ffab
Fabi-02 <fabian.klimpel2002@gmail.com>
2023-02-08 15:44:51 +0100

    added tree transition animations

--- src/components/d3/D3Test.vue
+++ src/components/d3/D3Test.vue
@@ -11,7 +11,8 @@ const data: TreeNode = {
     children: []
 }
 
-addChilds(data, 5);
+var depth = 1
+addChilds(data, depth);
 
 function addChilds(node: TreeNode, depth: number) {
     if (depth <= 0) return;
@@ -30,12 +31,20 @@ function addChilds(node: TreeNode, depth: number) {
 
 type TreeLayoutNode = HierarchyPointNode<TreeNode>;
 
+const width = 700;
+const height = 500;
+
+const padding = 20;
+
+const animationDuration = 1000;
+
+
+type GroupSelection = d3.Selection<SVGGElement, unknown, HTMLElement, any>;
+var linkGroup: GroupSelection | null = null
+var nodeGroup: GroupSelection | null = null;
+
 onMounted(() => {
 
-    const width = 700;
-    const height = 500;
-
-    const padding = 20;
 
     const svg = d3.select("#d3-test").insert("svg")
         .attr("height", "100%")
@@ -43,52 +52,109 @@ onMounted(() => {
         // .attr("preserveAspectRatio", "none")
         .attr("viewBox", `0 0 ${width} ${height}`);
 
-    const linkGroup = svg.insert("g").attr('class', 'tree-links');
-    const nodeGroup = svg.insert("g").attr('class', 'tree-nodes');
+    linkGroup = svg.insert("g").attr("class", "tree-links");
+    nodeGroup = svg.insert("g").attr("class", "tree-nodes");
 
-    const tree = d3.tree<TreeNode>()
+    update();
+});
+
+function update() {
+    let tree = d3.tree<TreeNode>()
         .size([width - padding * 2, height - padding * 2])
         .separation((a, b) => {
             return a.parent === b.parent ? 1 : 1
         });
 
-    const root = d3.hierarchy<TreeNode>(data);
-    const treeData = tree(root);
+    let root = d3.hierarchy<TreeNode>(data);
+    let treeData = tree(root);
 
     treeData.each((node) => {
         node.x += padding;
         node.y += padding;
     });
 
-    const links = treeData.links();
+    let links = treeData.links();
 
-    const linkPathGen = d3.linkVertical<SVGPathElement, TreeLayoutNode>().x(d => d.x).y(d => d.y);
+    let linkPathGen = d3.linkVertical<SVGPathElement, TreeLayoutNode>().x(d => d.x).y(d => d.y);
     // const linkPathGen = d3.link<SVGPathElement, TreeLayoutNode>(d3.curveBasis).x(d => d.x).y(d => d.y);
 
-    linkGroup.selectAll('path').data(links).enter()
-        .append('path').attr('d', linkPathGen as any);
+    let linkSelection = linkGroup?.selectAll(".tree-link").data(links)
+    linkSelection?.join(
+        enter => {
+            let linkEnter = enter.append("path").attr("class", "tree-link");
+                
+            linkEnter.attr("d", linkPathGen as any)
+                .attr("opacity", 0)
+                .transition()
+                .delay(animationDuration / 2)
+                .duration(animationDuration / 2)
+                .attr("opacity", 1);
 
-    const nodesEnter = nodeGroup.selectAll('g').data(treeData).enter()
-        .append("g")
-        .attr("class", "tree-node")
-        .attr("transform", (d) => `translate(${d.x},${d.y})`)
+            return linkEnter;
+        },
+        update => 
+            update.transition()
+            .duration(animationDuration / 2)
+            .attr("d", linkPathGen as any)
+            .attr("opacity", 1)
+    )
 
-    nodesEnter.append('circle')
-        .attr("r", 10)
-        .attr("fill", "#666");
+    let nodesSelection = nodeGroup?.selectAll(".tree-node").data(treeData);
+    nodesSelection?.join(
+        enter => {
+            let nodeEnter = enter.append("g").attr("class", "tree-node");
+            
+            nodeEnter.append("circle")
+                .attr("r", 10)
+                .attr("fill", "#666");
 
-    nodesEnter.append('text')
-        .attr("text-anchor", "middle")
-        .attr("dy", 5)
-        .attr("fill", "white")
-        .text((d) => `${d.data.id}`)
+            nodeEnter.append("text")
+                .attr("text-anchor", "middle")
+                .attr("dy", 5)
+                .attr("fill", "white")
+                .text(d => `${d.data.id}`)
 
-});
+            nodeEnter.attr("transform", d => `translate(${d.x},${d.y})`)
+                .attr("opacity", 0)
+                .transition()
+                .delay(animationDuration / 2)
+                .duration(animationDuration / 2)
+                .attr("opacity", 1);
+
+            return nodeEnter;
+        },
+        update => {
+            let nodeUpdate = update.transition()
+                .duration(animationDuration / 2)
+                .attr("transform", (d) => `translate(${d.x},${d.y})`)
+                .attr("opacity", 1);
+
+            nodeUpdate.select("text").text(d => `${d.data.id}`)
+            
+            return nodeUpdate;
+        }
+    )
+}
+
+function test() {
+    depth += 1;
+    id = 0;
+    data.children = []
+    addChilds(data, depth);
+
+    // data.children![0].children?.push({
+    //     id: ++id,
+    //     children: []
+    // })
+    update();
+}
 </script>
 
 
 <template>
-    <div id="d3-test"></div>
+    <div id="d3-test">
+        <button @click="test">Test</button>
+    </div>
 </template>
 
 
@@ -99,7 +165,7 @@ onMounted(() => {
     /* stroke-width: 0.1px; */
 }
 
-#d3-test > svg {
+#d3-test>svg {
     position: absolute;
 }
 </style>
